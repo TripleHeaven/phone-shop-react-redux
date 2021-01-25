e7d65223e28845d74423844853438446a6d1a501
Ilya <heaven30.buroff@gmail.com>
2021-01-25 23:46:08 +0300

    added rendering one phone detailed

--- src/containers/phone/index.js
+++ src/containers/phone/index.js
@@ -2,16 +2,91 @@ import React , {Component, useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPhoneById } from '../actions';
 import {connect} from 'react-redux';
+import { getPhoneById } from '../../selectors';
+import * as R from 'ramda';
+
 
 export default function Phones(props) {
   //const phone = useSelector(state => getPH)
+  const phone = useSelector(state => getPhoneById(state, state.phonePage.id));
   const dispatch  = useDispatch();
+ // const phone = useSelector(state => getPhoneById(state, state.phonePage.id));
   
   useEffect(() => {
     dispatch(fetchPhoneById(props.match.params.id));
+    //phone = useSelector(state => getPhoneById(state, state.phonePage.id));
+   
     // dispatchEvent(fetchPhoneById());
   }, []) ;
+  useEffect(() => {
+    //dispatch(fetchPhoneById(props.match.params.id));
+    //phone = useSelector(state => getPhoneById(state, state.phonePage.id));
+    console.log(phone);
+    // dispatchEvent(fetchPhoneById());
+  }, [phone]) ;
+  const renderFields = () =>  {
+    const {phonetoDraw} = phone;
+    const columnFields = R.compose(
+      R.toPairs,
+      R.pick([
+        'cpu', 
+        'camera',
+        'size',
+        'weight',
+        'display',
+        'memory'
+      ])
+    )(phone);
+    
+    return columnFields.map(([key, value])=> (
+      <div className="column" key={key}><div className="ab-details-title">
+        <p>{key}</p>
+        </div>
+        <div className='ab-details-info'>{value}</div>
+        </div>
+    ))
+  
+  };
+  const renderContent= () =>{
+    const {phoneToRender} = phone;
+     
+    return(
+      <div className="thumbnail">
+        <div className='row'>
+          <div className='col-md-6'>
+            <img className='img-thumbnail'
+            src = {phone.image}
+            alt={phone.name}/>
+          </div>
+          <div className='col-md-6'>
+            {renderFields()}
+          </div>
+        </div>
+        <div className='caption-full'>
+          <h4 className='pull-right'>${phone.price}</h4>
+          <h4>{phone.name}</h4>
+          <p>{phone.description}</p>
+        </div>
+      </div>
+    )
+  }
+  const renderSidebar= () =>{
+    return(
+      <div>Sidebar</div>
+    )
+  }
   return (
-    <div>Phone</div>
+    <div className='view-container'>
+      <div className='container'>
+        <div className='row'>
+          <div className= 'col-md-9'>
+            {phone && renderContent()}
+          </div>
+          <div className='col-md-3'>
+            {phone && renderSidebar()}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/reducers/phonePage.js
+++ src/reducers/phonePage.js
@@ -1,16 +1,14 @@
 import * as R from 'ramda';
 
 import {FETCH_PHONE_BY_ID_FAILURE, FETCH_PHONE_BY_ID_SUCCES} from '../containers/actionTypes';
-const initialState = {
-  id : null,
-};
+const initialState = {};
 
 export default (state = initialState, {type , payload}) => {
   switch(type) {
     case FETCH_PHONE_BY_ID_SUCCES:
       return R.merge(state, {
         id: R.prop('id', payload)
-      })
+      });
     default : 
       return state;
   }
